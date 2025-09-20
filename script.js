396e35ae1ae5feee61b849f64fdb1046e3726644
Ahmed Mohammed Jayanti Mochi <amochi670@rku.ac.in>
2025-09-20 23:06:59 +0530

    My Submitted Answer's

--- script.js
+++ script.js
@@ -8,20 +8,42 @@ let arr = [
 
 function PrintDeveloperbyMap() {
   //Write your code here , just console.log
+  arr.map((item) => {
+    if (item.profession === "developer") {
+      console.log(item);
+    }
+  })
 }
 
 function PrintDeveloperbyForEach() {
   //Write your code here , just console.log
+  arr.forEach((item) => {
+    if (item.profession === "developer") {
+      console.log(item);
+    }
+  })
 }
 
 function addData() {
   //Write your code here, just console.log
+  const newUser = { id: 4, name: "susan", age: "20", profession: "intern" };
+  arr.push(newUser);
+  console.log(arr[arr.length - 1]);
 }
 
 function removeAdmin() {
   //Write your code here, just console.log
+  arr = arr.filter((item) => item.profession !== "admin");
+  console.log(arr);
 }
 
 function concatenateArray() {
   //Write your code here, just console.log
+  const conArr = [
+    { id: 5, name: "ahmed", age: "22", profession: "developer" },
+    { id: 6, name: "mohammed", age: "62", profession: "electrical engineer" },
+    { id: 7, name: "moin", age: "24", profession: "chief" },
+  ];
+  const comb = arr.concat(conArr);
+  console.log(comb);
 }
